2ca79a234811ee6883965f18351c89910925bff2
Atyson Jaime <atysonjaime@gmail.com>
2022-12-02 02:23:45 -0300

    :sparkles: Adiciona componente Filtro por Data

--- components/Main/index.jsx
+++ components/Main/index.jsx
@@ -1,10 +1,10 @@
-//import styles from "./Main.module.scss";
-//import { Flex, Spacer, Box } from "@chakra-ui/react";
 import HeaderPage from "../HeaderPage";
+import FilterPorDate from "./components/FilterData";
 import styled from "styled-components";
 
 const StyledSection = styled.section`
   display: flex;
+  flex-direction: column;
   padding: 3.0625rem 1.875rem;
   margin-left: 18.125rem;
   width: calc(100% - 18.125rem);
@@ -30,6 +30,7 @@ export default function Main() {
   return (
     <StyledSection>
       <HeaderPage headerProps={headerProps} />
+      <FilterPorDate />
     </StyledSection>
   );
 }
